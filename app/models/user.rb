82e2064c4f4071330889e8b265bf07732a25de30
krk324 <krk32488@gmail.com>
2014-08-21 22:50:57 -0500

    Facebook authentication fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,16 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.username = auth.info.name   # assuming the user model has a name
+      user.save!
     end
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
 end
