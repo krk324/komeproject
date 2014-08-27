4b331a966b5bed451fa2cd168f5e22e6fccb4da1
krk324 <krk32488@gmail.com>
2014-08-26 21:38:38 -0500

    App now redirect user to sign-up page if not logged in.

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -89,6 +89,7 @@ var Hackmai = Hackmai || {};
     //check if logout link exist in the navbar.(check if user is logged in)
     if ($('.dropdown').attr('data-method') !== 'delete'){
       window.location.replace("/users/sign_in");
+      return;
     }
 
     if ($('#address').val() === "" || $('#phone-input').val() === "(   )    -    " ){
