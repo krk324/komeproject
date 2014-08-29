41d09ff5509a3bec99d516c68c76c36f5e93aa21
krk324 <krk32488@gmail.com>
2014-08-29 12:30:30 -0500

    Add animation plugin.

--- app/assets/javascripts/initializer.js
+++ app/assets/javascripts/initializer.js
@@ -3,5 +3,22 @@ var Hackmai = Hackmai || {};
 
   Hackmai.CartApp.initializer();
 
+  $(".animsition").animsition({
+    inClass               :   'fade-in',
+    outClass              :   'fade-out',
+    inDuration            :    1500,
+    outDuration           :    800,
+    linkElement           :   '.animsition-link',
+    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+    touchSupport          :    true,
+    loading               :    true,
+    loadingParentElement  :   'body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ]
+  });
+
 };
 
