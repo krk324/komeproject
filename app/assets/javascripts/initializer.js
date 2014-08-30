3a552f2ca830de30df905d6f7f9685f6095a7b8d
krk324 <krk32488@gmail.com>
2014-08-29 21:19:09 -0500

    Format order html.

--- app/assets/javascripts/initializer.js
+++ app/assets/javascripts/initializer.js
@@ -3,22 +3,25 @@ var Hackmai = Hackmai || {};
 
   Hackmai.CartApp.initializer();
 
-  $(".animsition").animsition({
-    inClass               :   'fade-in',
-    outClass              :   'fade-out',
-    inDuration            :    1500,
-    outDuration           :    800,
-    linkElement           :   '.animsition-link',
-    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-    touchSupport          :    true,
-    loading               :    true,
-    loadingParentElement  :   'body', //animsition wrapper element
-    loadingClass          :   'animsition-loading',
-    unSupportCss          : [ 'animation-duration',
-                              '-webkit-animation-duration',
-                              '-o-animation-duration'
-                            ]
+  $('#d1').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.7,
   });
+  // $(".animsition").animsition({
+  //   inClass               :   'zoom-in',
+  //   outClass              :   'zoom-out',
+  //   inDuration            :    900,
+  //   outDuration           :    800,
+  //   linkElement           :   '.animsition-link',
+  //   // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+  //   touchSupport          :    true,
+  //   loading               :    true,
+  //   loadingParentElement  :   'body', //animsition wrapper element
+  //   unSupportCss          : [ 'animation-duration',
+  //                             '-webkit-animation-duration',
+  //                             '-o-animation-duration'
+  //                           ]
+  // });
 
 };
 
