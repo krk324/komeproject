5c4e956ad8235fc55aac536da95dc8a8bd0effc5
krk324 <krk32488@gmail.com>
2014-08-31 13:35:41 -0500

    Unicorn setting.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,3 @@
-# Dir[File.join(Rails.root, 'app', 'workers', '*.rb')].each { |file|
-#   require file }
-
-# config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-# puts "Redis config is #{config}"
-
-# Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
-
 if ENV["REDISCLOUD_URL"]
   $redis = Resque.redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 else
