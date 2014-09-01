75a7f2730015513829aa53913eaf0e37ff714f5a
krk324 <krk32488@gmail.com>
2014-08-31 20:48:15 -0500

    Display username if account has one. Format email template.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,15 +9,18 @@ class UserMailer < ActionMailer::Base
   #
   def send_welcome_mail(user_id)
     @greeting = "Hi"
-
     @user = User.find(user_id)
-    mail(:to => "<#{@user.email}>", :subject => "Welcome to Hackmai <%=@user.email%>.")
+    @username = check_username(@user)
+    mail(:to => "<#{@user.email}>", :subject => "Welcome to Hackmai #{@username}!")
   end
 
   def send_order_confirmation(order_id,user_id)
     @order = Order.find(order_id)
     @user = User.find(user_id)
-
     mail(:to => "<#{@user.email}>", :subject => "Order Confirmation")
+  end
+
+  def check_username(user)
+    user.username ? user.username : user.email
   end
 end
