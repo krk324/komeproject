347aa76245d0d10b7682a4c3af439faad610da4e
krk324 <krk32488@gmail.com>
2014-09-05 14:44:12 -0400

    Add some description.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
 MenuItem.create(
-  name: 'Donburi',
+  name: 'Donburi (WHITE)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Donburi2',
+  name: 'Donburi2 (WHITE)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Donburi3',
+  name: 'Donburi3 (WHITE)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Sushi1',
+  name: 'Donburi (BROWN)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Sushi2',
+  name: 'Donburi2 (BROWN)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Sushi3',
+  name: 'Donburi3 (BROWN)',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
-  price: 9,
+  price: 10,
   description: 'Tasty Japanese bowl')
 
 # This file should contain all the record creation needed to seed the database with its default values.
