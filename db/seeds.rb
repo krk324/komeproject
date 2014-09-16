be9a44eddc942a8e3565c9e5e7399f69fc26aaba
krk324 <krk32488@gmail.com>
2014-09-16 16:16:17 -0400

    Change position of flash message.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,75 @@
 MenuItem.create(
-  name: 'Donburi (WHITE)',
+  name: 'HAKUMAI (WHITE RICE)',
+  image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+  price: 10,
+  description: 'Kagayaki Brand - Preminum white rice')
+
+MenuItem.create(
+  name: 'GENMAI (BROWN RICE)',
+  image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+  price: 10,
+  description: 'Akitakomachi Brand - Premium brown rice')
+
+MenuItem.create(
+  name: 'BLACK RICE',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Donburi2 (WHITE)',
+  name: 'Tuna & Avocado',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Donburi3 (WHITE)',
+  name: 'Beef Asparagus',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
 MenuItem.create(
-  name: 'Donburi (BROWN)',
+  name: 'Yuzu & Baby leaf',
   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
   price: 10,
   description: 'Tasty Japanese bowl')
 
-MenuItem.create(
-  name: 'Donburi2 (BROWN)',
-  image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
-  price: 10,
-  description: 'Tasty Japanese bowl')
+## V1 Rice and topping as one menu
+# MenuItem.create(
+#   name: 'Donburi (WHITE)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
 
-MenuItem.create(
-  name: 'Donburi3 (BROWN)',
-  image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
-  price: 10,
-  description: 'Tasty Japanese bowl')
+# MenuItem.create(
+#   name: 'Donburi2 (WHITE)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
+
+# MenuItem.create(
+#   name: 'Donburi3 (WHITE)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
+
+# MenuItem.create(
+#   name: 'Donburi (BROWN)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
+
+# MenuItem.create(
+#   name: 'Donburi2 (BROWN)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
+
+# MenuItem.create(
+#   name: 'Donburi3 (BROWN)',
+#   image_url:'http://www.cafecompany.co.jp/brands/2011/11/29/wC_ueno_20111126/negitoro_web.jpg',
+#   price: 10,
+#   description: 'Tasty Japanese bowl')
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
