a29297baeadb6d59db01e6491ba5cd279baf98ae
krk324 <krk32488@gmail.com>
2014-09-26 18:40:10 -0500

    Add production key to stripe config.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,13 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+if Rails.env == 'development' || Rails.env == 'staging'
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
+  }
+elsif Rails.env == 'production'
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY_LIVE'],
+    :secret_key      => ENV['SECRET_KEY_LIVE']
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
