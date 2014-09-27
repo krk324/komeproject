c84d09f35213a09ac9c303c298c38c0d42d062ad
krk324 <krk32488@gmail.com>
2014-09-27 14:01:34 -0500

    remove address id from order table.

--- db/migrate/20140808190216_create_orders.rb
+++ db/migrate/20140808190216_create_orders.rb
@@ -2,7 +2,6 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.references :user, index: true
-      t.references :address, index: true
       t.decimal :price, precision: 8, scale: 2
       t.decimal :tip, precision:8, scale: 2, default: 1
       t.boolean :is_purchased
