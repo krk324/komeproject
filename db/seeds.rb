5d8a37ef6f9d68c257fd6dae5314dc40f6ae4145
krk324 <krk32488@gmail.com>
2014-10-02 10:56:45 -0500

    Add quantity value to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,42 +2,42 @@ MenuItem.create(
   name: 'Brittle Salmon Bowl(W)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/brittlesalmon-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Salmon, Spicy Mayo, Tempura flakes')
 
 MenuItem.create(
   name: 'Yakiniku Beef Bowl(W)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/yakinikubeef-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Japanese style beef, Lettuce')
 
 MenuItem.create(
   name: 'Soy Chicken Avocado Bowl(W)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/chickenavocado-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Chicken, Avocado, Tomato')
 
 MenuItem.create(
   name: 'Brittle Salmon Bowl(B)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/brittlesalmon-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Salmon, Spicy Mayo, Tempura flakes')
 
 MenuItem.create(
   name: 'Yakiniku Beef Bowl(B)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/yakinikubeef-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Japanese style beef, Lettuce')
 
 MenuItem.create(
   name: 'Soy Chicken Avocado Bowl(B)',
   image_url:'https://s3-us-west-2.amazonaws.com/hackmai/chickenavocado-small.jpg',
   price: 12,
-  quantity: 0,
+  quantity: 20,
   description: 'Chicken, Avocado, Tomato')
 
 ## V1 Rice and topping as one menu
