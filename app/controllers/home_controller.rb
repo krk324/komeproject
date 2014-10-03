e89e19f2cfcd2610fe1082c646b1e0f1ee8f43e0
krk324 <krk32488@gmail.com>
2014-10-03 13:55:30 -0500

    Redirect user to order page if logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   #before_action :current_or_guest_user
+  before_action :check_login
+
 
   def index
+  end
+
+  private
+
+  def check_login
+    redirect_to orders_path if logged_in?
   end
 end
