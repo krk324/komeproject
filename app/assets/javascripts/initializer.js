d87ca92a091cad8d6778200e543aa6ce872963ce
krk324 <krk32488@gmail.com>
2014-10-05 11:49:48 -0500

    Move delivery area to tab.

--- app/assets/javascripts/initializer.js
+++ app/assets/javascripts/initializer.js
@@ -13,17 +13,19 @@ var Hackmai = Hackmai || {};
   //google map delivery area
   function initialize() {
     var chicago = new google.maps.LatLng(41.875696,-87.624207);
-    var mapOptions = {
-      zoom: 30,
-      center: chicago
-    };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'));
 
     var ctaLayer = new google.maps.KmlLayer({
       url: 'https://s3-us-west-2.amazonaws.com/hackmai/03102014-4z87fde9.kml'
     });
     ctaLayer.setMap(map);
+
+  $("#mapModal").on("shown.bs.modal", function(e) {
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(chicago);
+    map.setZoom(14);
+  });
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
--- app/assets/javascripts/show_modal.js
+++ app/assets/javascripts/show_modal.js
@@ -14,11 +14,15 @@ var Hackmai = Hackmai || {};
   showTerms: function(){
     $('#termsModal').modal('show');
   },
+  showMap: function(){
+    $('#mapModal').modal('show');
+  },
   initializer: function(event){
     $('#about-link').on('click', this.showAboutus.bind(this));
     $('#contact-link').on('click', this.showContact.bind(this));
     $('#privacy-link').on('click', this.showPrivacy.bind(this));
     $('#terms-link').on('click', this.showTerms.bind(this));
+    $('#map-link').on('click', this.showMap.bind(this));
 
   }
 };
