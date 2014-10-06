bcbe0f4f846838f6073d60af1fd3cb737fc554f9
krk324 <krk32488@gmail.com>
2014-10-06 11:54:20 -0500

    Restrict geo auto complete to geolocation result.

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -127,7 +127,7 @@ var Hackmai = Hackmai || {};
         if (!bounds.contains(results[0].geometry.location)){
           debugger;
           flag2 = 1;
-          alertify.error("We currently don't delivery to your area. Will expand soon!");
+          alertify.error("We currently don't delivery to your area.");
         }
       }
       else {
