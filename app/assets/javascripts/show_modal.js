8f3677f1aa0413d50b6c651fbd280b03390c917c
krk324 <krk32488@gmail.com>
2014-10-18 11:59:04 -0500

    Add large order modal

--- app/assets/javascripts/show_modal.js
+++ app/assets/javascripts/show_modal.js
@@ -17,12 +17,16 @@ var Hackmai = Hackmai || {};
   showMap: function(){
     $('#mapModal').modal('show');
   },
+  showLargeOrders: function(){
+    $('#largeModal').modal('show');
+  },
   initializer: function(event){
     $('#about-link').on('click', this.showAboutus.bind(this));
     $('#contact-link').on('click', this.showContact.bind(this));
     $('#privacy-link').on('click', this.showPrivacy.bind(this));
     $('#terms-link').on('click', this.showTerms.bind(this));
     $('#map-link').on('click', this.showMap.bind(this));
+    $('[id="large-link"]').on('click', this.showLargeOrders.bind(this));
 
   }
 };
