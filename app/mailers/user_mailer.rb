1f34faa498ef2556812ecfaf9413bf5c3f020c07
krk324 <krk32488@gmail.com>
2014-10-29 14:06:27 -0500

    App now send out of stock email when out of stock.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,13 +10,19 @@ class UserMailer < ActionMailer::Base
   def send_welcome_mail(user_id)
     @user = User.find(user_id)
     @username = check_username(@user)
-    mail(:to => "<#{@user.email}>", :subject => "Welcome to Hackmai #{@username}!")
+    mail(:to => "<#{@user.email}>", :subject => "Welcome to HACKMAI!")
   end
 
   def send_order_confirmation(order_id,user_id)
     @order = Order.find(order_id)
     @user = User.find(user_id)
-    mail(:to => "<#{@user.email}>", :subject => "Order Confirmation")
+    mail(:to => "<#{@user.email}>", :subject => "HACKMAI - Order Confirmation")
+  end
+
+  def send_order_error(order_id,user_id)
+    @order = Order.find(order_id)
+    @user = User.find(user_id)
+    mail(:to => "<#{@user.email}>", :subject => "HACKMAI - We're Sorry Out of Stock")
   end
 
   def check_username(user)
