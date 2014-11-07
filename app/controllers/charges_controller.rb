321d55c1c98d1a806c214454be3b7304db51fad2
krk324 <krk32488@gmail.com>
2014-11-07 10:49:10 -0600

    Dont use resque for email for now.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,7 +10,7 @@ class ChargesController < ApplicationController
       redirect_to orders_path
       raise "We're sorry one or more of your items ordered
           is out of stock. Please order again from the order page. *Payment has not been processed."
-      UserMailer.send_order_error(@order.id,current_user.id).deliver
+      UserMailer.send_order_error(@order.id,current_user.id).deliver!
       return
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,10 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :async,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+         #,:async
 
   validates_presence_of :username
   after_create :send_new_user_mail
