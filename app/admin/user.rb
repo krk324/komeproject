f2d3d0a83401538fa9132a70586b64888c00fd62
krk324 <krk32488@gmail.com>
2014-11-08 14:15:35 -0600

    Add username when creating user in adminpage

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation, :role
+  permit_params :email, :password, :password_confirmation, :role, :username
 
   index do
     column :email
@@ -15,6 +15,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "User Details" do
       f.input :email
+      f.input :username
       f.input :password
       f.input :password_confirmation
       f.input :role, as: :radio, collection: {Customer: "customer", Driver: "driver", Administrator: "administrator"}
