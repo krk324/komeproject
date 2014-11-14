19cf64f0f25508a990561fa9fe37d4df198cc2e0
krk324 <krk32488@gmail.com>
2014-11-13 19:37:32 -0600

    Change drivers orderpage sorting method.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,7 @@
 class DriversController < ApplicationController
 
   def index
-    @orders = Order.order('created_at DESC').all
+    @orders = Order.order('created_at DESC').where(is_purchased: true)
     authorize! :read, @orders
   end
 
