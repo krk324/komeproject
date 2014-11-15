20b00712514a742acb68f96206a6910dd1ad4620
krk324 <krk32488@gmail.com>
2014-11-15 10:29:33 -0600

    Only show todays order on driver's page.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,7 @@
 class DriversController < ApplicationController
 
   def index
-    @orders = Order.order('created_at DESC').where(is_purchased: true)
+    @orders = Order.order('created_at DESC').where(is_purchased: true, created_at: Date.today)
     authorize! :read, @orders
   end
 
